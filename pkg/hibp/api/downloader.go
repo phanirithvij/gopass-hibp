249f3f2723e8111b433238b080d87bc7adc70836
Phani Rithvij <phanirithvij2000@gmail.com>
2024-06-09 07:28:25 +0530

    more
    Signed-off-by: Phani Rithvij <phanirithvij2000@gmail.com>

--- pkg/hibp/api/downloader.go
+++ pkg/hibp/api/downloader.go
@@ -47,7 +47,7 @@ func Download(ctx context.Context, path string, keep bool) error {
 
 	sem := make(chan struct{}, runtime.NumCPU()*4)
 	wg := &sync.WaitGroup{}
-	for i := 0; i < max; i++ {
+	for i := range max {
 		wg.Add(1)
 		go func() {
 			sem <- struct{}{}
